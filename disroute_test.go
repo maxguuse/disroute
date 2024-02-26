1de59a8c1e06870e669d79f259b91512d640722a
maxguuse <max.pink.goose@gmail.com>
2024-02-26 16:51:30 +0300

    dev: refactored tests

--- disroute_test.go
+++ disroute_test.go
@@ -23,6 +23,232 @@ var (
 	}
 )
 
+type table struct {
+	cmds         []*disroute.Cmd
+	interactions []*discordgo.InteractionCreate
+}
+
+func getSingleCmd(iType discordgo.InteractionType) table {
+	cmds := []*disroute.Cmd{
+		{
+			Path: "cmd",
+			Handlers: disroute.Handlers{
+				Cmd:          EmptyHandler,
+				Autocomplete: EmptyHandler,
+			},
+		},
+	}
+
+	i := []*discordgo.InteractionCreate{
+		{
+			Interaction: &discordgo.Interaction{
+				Type: iType,
+				Data: discordgo.ApplicationCommandInteractionData{
+					Name: "cmd",
+				},
+			},
+		},
+	}
+
+	return table{
+		cmds:         cmds,
+		interactions: i,
+	}
+}
+
+func getSubcommandCmd(iType discordgo.InteractionType) table {
+	cmds := []*disroute.Cmd{
+		{
+			Path: "sub",
+			Options: []*disroute.CmdOption{
+				{
+					Path: "cmd",
+					Type: disroute.TypeSubcommand,
+					Handlers: disroute.Handlers{
+						Cmd:          EmptyHandler,
+						Autocomplete: EmptyHandler,
+					},
+				},
+				{
+					Path: "cmd2",
+					Type: disroute.TypeSubcommand,
+					Handlers: disroute.Handlers{
+						Cmd:          EmptyHandler,
+						Autocomplete: EmptyHandler,
+					},
+				},
+			},
+		},
+	}
+	i := []*discordgo.InteractionCreate{
+		{
+			Interaction: &discordgo.Interaction{
+				Type: iType,
+				Data: discordgo.ApplicationCommandInteractionData{
+					Name: "sub",
+					Options: []*discordgo.ApplicationCommandInteractionDataOption{
+						{
+							Name: "cmd",
+							Type: discordgo.ApplicationCommandOptionSubCommand,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	return table{
+		cmds:         cmds,
+		interactions: i,
+	}
+}
+
+func getSubcommandGroupCmd(iType discordgo.InteractionType) table {
+	cmds := []*disroute.Cmd{
+		{
+			Path: "gr-sub",
+			Options: []*disroute.CmdOption{
+				{
+					Path: "gr",
+					Type: disroute.TypeSubcommandGroup,
+					Options: []*disroute.CmdOption{
+						{
+							Path: "cmd",
+							Type: disroute.TypeSubcommand,
+							Handlers: disroute.Handlers{
+								Cmd:          EmptyHandler,
+								Autocomplete: EmptyHandler,
+							},
+						},
+						{
+							Path: "cmd2",
+							Type: disroute.TypeSubcommand,
+							Handlers: disroute.Handlers{
+								Cmd:          EmptyHandler,
+								Autocomplete: EmptyHandler,
+							},
+						},
+						{
+							Path: "non_subcommand",
+							Type: discordgo.ApplicationCommandOptionBoolean,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	i := []*discordgo.InteractionCreate{
+		{
+			Interaction: &discordgo.Interaction{
+				Type: iType,
+				Data: discordgo.ApplicationCommandInteractionData{
+					Name: "gr-sub",
+					Options: []*discordgo.ApplicationCommandInteractionDataOption{
+						{
+							Name: "gr",
+							Type: discordgo.ApplicationCommandOptionSubCommandGroup,
+							Options: []*discordgo.ApplicationCommandInteractionDataOption{
+								{
+									Name: "cmd",
+									Type: discordgo.ApplicationCommandOptionSubCommand,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	return table{
+		cmds:         cmds,
+		interactions: i,
+	}
+}
+
+func getMixedCmd(iType discordgo.InteractionType) table {
+	cmds := []*disroute.Cmd{
+		{
+			Path: "gr-sub",
+			Options: []*disroute.CmdOption{
+				{
+					Path: "gr",
+					Type: disroute.TypeSubcommandGroup,
+					Options: []*disroute.CmdOption{
+						{
+							Path: "cmd",
+							Type: disroute.TypeSubcommand,
+							Handlers: disroute.Handlers{
+								Cmd:          EmptyHandler,
+								Autocomplete: EmptyHandler,
+							},
+						},
+						{
+							Path: "cmd2",
+							Type: disroute.TypeSubcommand,
+							Handlers: disroute.Handlers{
+								Cmd:          EmptyHandler,
+								Autocomplete: EmptyHandler,
+							},
+						},
+					},
+				},
+				{
+					Path: "cmd",
+					Type: disroute.TypeSubcommand,
+					Handlers: disroute.Handlers{
+						Cmd:          EmptyHandler,
+						Autocomplete: EmptyHandler,
+					},
+				},
+			},
+		},
+	}
+
+	i := []*discordgo.InteractionCreate{
+		{
+			Interaction: &discordgo.Interaction{
+				Type: iType,
+				Data: discordgo.ApplicationCommandInteractionData{
+					Name: "gr-sub",
+					Options: []*discordgo.ApplicationCommandInteractionDataOption{
+						{
+							Name: "cmd",
+							Type: discordgo.ApplicationCommandOptionSubCommand,
+						},
+					},
+				},
+			},
+		},
+		{
+			Interaction: &discordgo.Interaction{
+				Type: iType,
+				Data: discordgo.ApplicationCommandInteractionData{
+					Name: "gr-sub",
+					Options: []*discordgo.ApplicationCommandInteractionDataOption{
+						{
+							Name: "gr",
+							Type: discordgo.ApplicationCommandOptionSubCommandGroup,
+							Options: []*discordgo.ApplicationCommandInteractionDataOption{
+								{
+									Name: "cmd",
+									Type: discordgo.ApplicationCommandOptionSubCommand,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	return table{
+		cmds:         cmds,
+		interactions: i,
+	}
+}
+
 func Test_RegisterAll_Errors(t *testing.T) {
 	r := disroute.New()
 
@@ -54,16 +280,9 @@ func Test_RegisterAll_Errors(t *testing.T) {
 func Test_RegisterAll_SingleCmd(t *testing.T) {
 	r := disroute.New()
 
-	cmds := []*disroute.Cmd{
-		{
-			Path: "cmd",
-			Handlers: disroute.Handlers{
-				Cmd: EmptyHandler,
-			},
-		},
-	}
+	cases := getSingleCmd(0)
 
-	err := r.RegisterAll(cmds)
+	err := r.RegisterAll(cases.cmds)
 	if err != nil {
 		t.Errorf("Expected nil error, got %v", err)
 	}
@@ -76,29 +295,9 @@ func Test_RegisterAll_SingleCmd(t *testing.T) {
 func Test_RegisterAll_Subcommands(t *testing.T) {
 	r := disroute.New()
 
-	cmds := []*disroute.Cmd{
-		{
-			Path: "sub",
-			Options: []*disroute.CmdOption{
-				{
-					Path: "cmd",
-					Type: disroute.TypeSubcommand,
-					Handlers: disroute.Handlers{
-						Cmd: EmptyHandler,
-					},
-				},
-				{
-					Path: "cmd2",
-					Type: disroute.TypeSubcommand,
-					Handlers: disroute.Handlers{
-						Cmd: EmptyHandler,
-					},
-				},
-			},
-		},
-	}
+	cases := getSubcommandCmd(0)
 
-	err := r.RegisterAll(cmds)
+	err := r.RegisterAll(cases.cmds)
 	if err != nil {
 		t.Errorf("Expected nil error, got %v", err)
 	}
@@ -111,35 +310,9 @@ func Test_RegisterAll_Subcommands(t *testing.T) {
 func Test_RegisterAll_SubcommandGroups(t *testing.T) {
 	r := disroute.New()
 
-	cmds := []*disroute.Cmd{
-		{
-			Path: "gr-sub",
-			Options: []*disroute.CmdOption{
-				{
-					Path: "gr",
-					Type: disroute.TypeSubcommandGroup,
-					Options: []*disroute.CmdOption{
-						{
-							Path: "cmd",
-							Type: disroute.TypeSubcommand,
-							Handlers: disroute.Handlers{
-								Cmd: EmptyHandler,
-							},
-						},
-						{
-							Path: "cmd2",
-							Type: disroute.TypeSubcommand,
-							Handlers: disroute.Handlers{
-								Cmd: EmptyHandler,
-							},
-						},
-					},
-				},
-			},
-		},
-	}
+	cases := getSubcommandGroupCmd(0)
 
-	err := r.RegisterAll(cmds)
+	err := r.RegisterAll(cases.cmds)
 	if err != nil {
 		t.Error(err)
 	}
@@ -148,42 +321,9 @@ func Test_RegisterAll_SubcommandGroups(t *testing.T) {
 func Test_RegisterAll_MixedSubcommands(t *testing.T) {
 	r := disroute.New()
 
-	cmds := []*disroute.Cmd{
-		{
-			Path: "gr-sub",
-			Options: []*disroute.CmdOption{
-				{
-					Path: "gr",
-					Type: disroute.TypeSubcommandGroup,
-					Options: []*disroute.CmdOption{
-						{
-							Path: "cmd",
-							Type: disroute.TypeSubcommand,
-							Handlers: disroute.Handlers{
-								Cmd: EmptyHandler,
-							},
-						},
-						{
-							Path: "cmd2",
-							Type: disroute.TypeSubcommand,
-							Handlers: disroute.Handlers{
-								Cmd: EmptyHandler,
-							},
-						},
-					},
-				},
-				{
-					Path: "cmd",
-					Type: disroute.TypeSubcommand,
-					Handlers: disroute.Handlers{
-						Cmd: EmptyHandler,
-					},
-				},
-			},
-		},
-	}
+	cases := getMixedCmd(0)
 
-	err := r.RegisterAll(cmds)
+	err := r.RegisterAll(cases.cmds)
 	if err != nil {
 		t.Error(err)
 	}
@@ -271,125 +411,53 @@ func Test_FindAndExecute_Errors(t *testing.T) {
 func Test_FindAndExecute_SingleCmd(t *testing.T) {
 	r := disroute.New()
 
-	cmds := []*disroute.Cmd{
-		{
-			Path: "cmd",
-			Handlers: disroute.Handlers{
-				Cmd: EmptyHandler,
-			},
-		},
-	}
+	cases := getSingleCmd(disroute.TypeCommand)
 
-	err := r.RegisterAll(cmds)
+	err := r.RegisterAll(cases.cmds)
 	if err != nil {
 		t.Errorf("Expected nil error, got %v", err)
 	}
 
-	_, err = r.FindAndExecute(&discordgo.InteractionCreate{
-		Interaction: &discordgo.Interaction{
-			Type: discordgo.InteractionApplicationCommand,
-			Data: discordgo.ApplicationCommandInteractionData{
-				Name: "cmd",
-			},
-		},
-	})
-	if err != nil {
-		t.Error("Expected nil error, got", err)
+	for _, i := range cases.interactions {
+		_, err = r.FindAndExecute(i)
+		if err != nil {
+			t.Error("Expected nil error, got", err)
+		}
 	}
 }
 
 func Test_FindAndExecute_Subcommand(t *testing.T) {
 	r := disroute.New()
 
-	cmds := []*disroute.Cmd{
-		{
-			Path: "cmd3",
-			Options: []*disroute.CmdOption{
-				{
-					Path: "sub",
-					Type: disroute.TypeSubcommand,
-					Handlers: disroute.Handlers{
-						Cmd: EmptyHandler,
-					},
-				},
-			},
-		},
-	}
+	cases := getSubcommandCmd(disroute.TypeCommand)
 
-	err := r.RegisterAll(cmds)
+	err := r.RegisterAll(cases.cmds)
 	if err != nil {
 		t.Errorf("Expected nil error, got %v", err)
 	}
 
-	_, err = r.FindAndExecute(&discordgo.InteractionCreate{
-		Interaction: &discordgo.Interaction{
-			Type: discordgo.InteractionApplicationCommand,
-			Data: discordgo.ApplicationCommandInteractionData{
-				Name: "cmd3",
-				Options: []*discordgo.ApplicationCommandInteractionDataOption{
-					{
-						Name: "sub",
-						Type: discordgo.ApplicationCommandOptionSubCommand,
-					},
-				},
-			},
-		},
-	})
-	if err != nil {
-		t.Error("Expected nil error, got", err)
+	for _, i := range cases.interactions {
+		_, err = r.FindAndExecute(i)
+		if err != nil {
+			t.Error("Expected nil error, got", err)
+		}
 	}
 }
 func Test_FindAndExecute_SubcommandGroup(t *testing.T) {
 	r := disroute.New()
 
-	cmds := []*disroute.Cmd{
-		{
-			Path: "cmd4",
-			Options: []*disroute.CmdOption{
-				{
-					Path: "gr",
-					Type: disroute.TypeSubcommandGroup,
-					Options: []*disroute.CmdOption{
-						{
-							Path: "sub2",
-							Type: disroute.TypeSubcommand,
-							Handlers: disroute.Handlers{
-								Cmd: EmptyHandler,
-							},
-						},
-					},
-				},
-			},
-		},
-	}
+	cases := getSubcommandGroupCmd(disroute.TypeCommand)
 
-	err := r.RegisterAll(cmds)
+	err := r.RegisterAll(cases.cmds)
 	if err != nil {
 		t.Errorf("Expected nil error, got %v", err)
 	}
 
-	_, err = r.FindAndExecute(&discordgo.InteractionCreate{
-		Interaction: &discordgo.Interaction{
-			Type: discordgo.InteractionApplicationCommand,
-			Data: discordgo.ApplicationCommandInteractionData{
-				Name: "cmd4",
-				Options: []*discordgo.ApplicationCommandInteractionDataOption{
-					{
-						Name: "gr",
-						Type: discordgo.ApplicationCommandOptionSubCommandGroup,
-						Options: []*discordgo.ApplicationCommandInteractionDataOption{
-							{
-								Name: "sub2",
-								Type: discordgo.ApplicationCommandOptionSubCommand,
-							},
-						},
-					},
-				},
-			},
-		},
-	})
-	if err != nil {
-		t.Error("Expected nil error, got", err)
+	for _, i := range cases.interactions {
+		_, err = r.FindAndExecute(i)
+		if err != nil {
+			t.Error("Expected nil error, got", err)
+		}
 	}
 }
 func Test_FindAndAutocomplete_Errors(t *testing.T) {
@@ -470,127 +538,52 @@ func Test_FindAndAutocomplete_Errors(t *testing.T) {
 func Test_FindAndAutocomplete_SingleCmd(t *testing.T) {
 	r := disroute.New()
 
-	cmds := []*disroute.Cmd{
-		{
-			Path: "cmd",
-			Handlers: disroute.Handlers{
-				Cmd:          EmptyHandler,
-				Autocomplete: EmptyHandler,
-			},
-		},
-	}
+	cases := getSingleCmd(disroute.TypeCommandAutocompletion)
 
-	err := r.RegisterAll(cmds)
+	err := r.RegisterAll(cases.cmds)
 	if err != nil {
 		t.Errorf("Expected nil error, got %v", err)
 	}
 
-	_, err = r.FindAndAutocomplete(&discordgo.InteractionCreate{
-		Interaction: &discordgo.Interaction{
-			Type: discordgo.InteractionApplicationCommandAutocomplete,
-			Data: discordgo.ApplicationCommandInteractionData{
-				Name: "cmd",
-			},
-		},
-	})
-	if err != nil {
-		t.Error("Expected nil error, got", err)
+	for _, i := range cases.interactions {
+		_, err = r.FindAndAutocomplete(i)
+		if err != nil {
+			t.Error("Expected nil error, got", err)
+		}
 	}
 }
 
 func Test_FindAndAutocomplete_Subcommand(t *testing.T) {
 	r := disroute.New()
 
-	cmds := []*disroute.Cmd{
-		{
-			Path: "cmd3",
-			Options: []*disroute.CmdOption{
-				{
-					Path: "sub",
-					Type: disroute.TypeSubcommand,
-					Handlers: disroute.Handlers{
-						Cmd:          EmptyHandler,
-						Autocomplete: EmptyHandler,
-					},
-				},
-			},
-		},
-	}
+	cases := getSubcommandCmd(disroute.TypeCommandAutocompletion)
 
-	err := r.RegisterAll(cmds)
+	err := r.RegisterAll(cases.cmds)
 	if err != nil {
 		t.Errorf("Expected nil error, got %v", err)
 	}
 
-	_, err = r.FindAndAutocomplete(&discordgo.InteractionCreate{
-		Interaction: &discordgo.Interaction{
-			Type: discordgo.InteractionApplicationCommandAutocomplete,
-			Data: discordgo.ApplicationCommandInteractionData{
-				Name: "cmd3",
-				Options: []*discordgo.ApplicationCommandInteractionDataOption{
-					{
-						Name: "sub",
-						Type: discordgo.ApplicationCommandOptionSubCommand,
-					},
-				},
-			},
-		},
-	})
-	if err != nil {
-		t.Error("Expected nil error, got", err)
+	for _, i := range cases.interactions {
+		_, err = r.FindAndAutocomplete(i)
+		if err != nil {
+			t.Error("Expected nil error, got", err)
+		}
 	}
 }
 func Test_FindAndAutocomplete_SubcommandGroup(t *testing.T) {
 	r := disroute.New()
 
-	cmds := []*disroute.Cmd{
-		{
-			Path: "cmd4",
-			Options: []*disroute.CmdOption{
-				{
-					Path: "gr",
-					Type: disroute.TypeSubcommandGroup,
-					Options: []*disroute.CmdOption{
-						{
-							Path: "sub2",
-							Type: disroute.TypeSubcommand,
-							Handlers: disroute.Handlers{
-								Cmd:          EmptyHandler,
-								Autocomplete: EmptyHandler,
-							},
-						},
-					},
-				},
-			},
-		},
-	}
+	cases := getSubcommandGroupCmd(disroute.TypeCommandAutocompletion)
 
-	err := r.RegisterAll(cmds)
+	err := r.RegisterAll(cases.cmds)
 	if err != nil {
 		t.Errorf("Expected nil error, got %v", err)
 	}
 
-	_, err = r.FindAndAutocomplete(&discordgo.InteractionCreate{
-		Interaction: &discordgo.Interaction{
-			Type: discordgo.InteractionApplicationCommandAutocomplete,
-			Data: discordgo.ApplicationCommandInteractionData{
-				Name: "cmd4",
-				Options: []*discordgo.ApplicationCommandInteractionDataOption{
-					{
-						Name: "gr",
-						Type: discordgo.ApplicationCommandOptionSubCommandGroup,
-						Options: []*discordgo.ApplicationCommandInteractionDataOption{
-							{
-								Name: "sub2",
-								Type: discordgo.ApplicationCommandOptionSubCommand,
-							},
-						},
-					},
-				},
-			},
-		},
-	})
-	if err != nil {
-		t.Error("Expected nil error, got", err)
+	for _, i := range cases.interactions {
+		_, err = r.FindAndAutocomplete(i)
+		if err != nil {
+			t.Error("Expected nil error, got", err)
+		}
 	}
 }
