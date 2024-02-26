6d9f81202beea480a95cda7bc02e0bc859cda88c
maxguuse <max.pink.goose@gmail.com>
2024-02-26 16:04:39 +0300

    feat: add option to add autocompletion handler to command

--- disroute.go
+++ disroute.go
@@ -9,6 +9,9 @@ import (
 )
 
 const (
+	TypeCommand               = discordgo.InteractionApplicationCommand
+	TypeCommandAutocompletion = discordgo.InteractionApplicationCommandAutocomplete
+
 	TypeSubcommand      = discordgo.ApplicationCommandOptionSubCommand
 	TypeSubcommandGroup = discordgo.ApplicationCommandOptionSubCommandGroup
 )
@@ -21,99 +24,100 @@ type HandlerFunc func(
 ) (string, error)
 
 type Cmd struct {
-	Path    string
-	Handler HandlerFunc
-	Options []*CmdOption
+	Path     string
+	Handlers Handlers
+	Options  []*CmdOption
 }
 
 type CmdOption struct {
-	Path    string
-	Handler HandlerFunc
-	Type    discordgo.ApplicationCommandOptionType
-	Options []*CmdOption
+	Path     string
+	Handlers Handlers
+	Type     discordgo.ApplicationCommandOptionType
+	Options  []*CmdOption
+}
+
+type Handlers struct {
+	Cmd          HandlerFunc
+	Autocomplete HandlerFunc
 }
 
 type Router struct {
 	cmdMx sync.RWMutex
 	cmds  map[string]HandlerFunc
+
+	autocompleteMx sync.RWMutex
+	autocompletes  map[string]HandlerFunc
 }
 
 func New() *Router {
 	return &Router{
-		cmds: make(map[string]HandlerFunc),
+		cmds:          make(map[string]HandlerFunc),
+		autocompletes: make(map[string]HandlerFunc),
 	}
 }
 
 func (r *Router) RegisterAll(cmds []*Cmd) error {
-	if len(cmds) == 0 {
-		return nil
-	}
-
-	r.cmdMx.Lock()
-	defer r.cmdMx.Unlock()
-
 	for _, cmd := range cmds {
 		pathParts := []string{cmd.Path}
 
-		if cmd.Handler == nil && len(cmd.Options) == 0 {
+		if cmd.Handlers.Cmd == nil && len(cmd.Options) == 0 {
 			return errors.New("cmd has no handler and no subcommands")
 		}
 
 		if len(cmd.Options) == 0 {
-			r.cmds[cmd.Path] = cmd.Handler
-			continue
+			r.registerCmd(pathParts, cmd.Handlers)
 		}
 
 		for _, opt := range cmd.Options {
-			if opt.Type != TypeSubcommand && opt.Type != TypeSubcommandGroup {
-				continue
-			}
-
 			pathParts = append(pathParts, opt.Path)
 
 			if opt.Type == TypeSubcommand {
-				if opt.Handler == nil {
+				if opt.Handlers.Cmd == nil {
 					return errors.New("subcommand has no handler")
 				}
 
-				p := strings.Join(pathParts, ":")
-				h := opt.Handler
-
-				r.cmds[p] = h
-
-				pathParts = pathParts[:len(pathParts)-1]
+				r.registerCmd(pathParts, opt.Handlers)
 			}
 
 			if opt.Type == TypeSubcommandGroup {
-				if len(opt.Options) == 0 {
-					return errors.New("subcommand group has no subcommands")
-				}
-
 				for _, sub := range opt.Options {
 					if sub.Type != TypeSubcommand {
 						continue
 					}
 
-					if sub.Handler == nil {
+					if sub.Handlers.Cmd == nil {
 						return errors.New("subcommand has no handler")
 					}
 
 					pathParts = append(pathParts, sub.Path)
 
-					p := strings.Join(pathParts, ":")
-					h := sub.Handler
-
-					r.cmds[p] = h
+					r.registerCmd(pathParts, sub.Handlers)
 
 					pathParts = pathParts[:len(pathParts)-1]
 				}
-
-				pathParts = pathParts[:len(pathParts)-1]
 			}
+
+			pathParts = pathParts[:len(pathParts)-1]
 		}
 	}
 
 	return nil
+}
+
+func (r *Router) registerCmd(pathParts []string, hs Handlers) {
+	path := strings.Join(pathParts, ":")
+
+	r.cmdMx.Lock()
+	defer r.cmdMx.Unlock()
+
+	r.cmds[path] = hs.Cmd
+
+	if hs.Autocomplete != nil {
+		r.autocompleteMx.Lock()
+		defer r.autocompleteMx.Unlock()
+
+		r.autocompletes[path] = hs.Autocomplete
+	}
 }
 
 func (r *Router) GetAll() map[string]HandlerFunc {
@@ -124,46 +128,74 @@ func (r *Router) GetAll() map[string]HandlerFunc {
 }
 
 func (r *Router) FindAndExecute(i *discordgo.InteractionCreate) (string, error) {
-	if i.Type != discordgo.InteractionApplicationCommand {
+	if i.Type != TypeCommand {
 		return "", errors.New("invalid interaction type")
 	}
 
 	r.cmdMx.RLock()
 	defer r.cmdMx.RUnlock()
 
-	data := i.ApplicationCommandData()
+	hd := r.buildHandlerData(i)
 
-	pathParts := []string{data.Name}
-	options := r.buildOptionsMap(data.Options)
+	if h, ok := r.cmds[hd.path]; ok {
+		return h(i.Interaction, hd.opts)
+	}
 
-	for _, opt := range data.Options {
-		if opt.Type == TypeSubcommand {
-			pathParts = append(pathParts, opt.Name)
-			options = r.buildOptionsMap(opt.Options)
-			break
-		}
+	return "", errors.New("command not registered")
+}
 
-		if opt.Type == TypeSubcommandGroup {
-			pathParts = append(pathParts, opt.Name)
-			for _, subOpt := range opt.Options {
-				if subOpt.Type == TypeSubcommand {
-					pathParts = append(pathParts, subOpt.Name)
-					options = r.buildOptionsMap(subOpt.Options)
-					break
-				}
-			}
+func (r *Router) FindAndAutocomplete(i *discordgo.InteractionCreate) (string, error) {
+	if i.Type != TypeCommandAutocompletion {
+		return "", errors.New("invalid interaction type")
+	}
+
+	r.autocompleteMx.RLock()
+	defer r.autocompleteMx.RUnlock()
+
+	hd := r.buildHandlerData(i)
+
+	if h, ok := r.autocompletes[hd.path]; ok {
+		return h(i.Interaction, hd.opts)
+	}
+
+	return "", errors.New("autocompletion not registered")
+}
+
+type handlerData struct {
+	path string
+	opts map[string]*DiscordCmdOption
+}
+
+func (r *Router) buildHandlerData(i *discordgo.InteractionCreate) *handlerData {
+	d := i.ApplicationCommandData()
+
+	pathParts := []string{d.Name}
+	options := r.buildOptionsMap(d.Options)
+
+	if len(d.Options) == 0 {
+		return &handlerData{
+			path: strings.Join(pathParts, ":"),
+			opts: options,
 		}
 	}
 
-	path := strings.Join(pathParts, ":")
-
-	var h HandlerFunc
-	var ok bool
-	if h, ok = r.cmds[path]; !ok {
-		return "", errors.New("command not registered")
+	if d.Options[0].Type == TypeSubcommand {
+		pathParts = append(pathParts, d.Options[0].Name)
+		options = r.buildOptionsMap(d.Options[0].Options)
 	}
 
-	return h(i.Interaction, options)
+	if d.Options[0].Type == TypeSubcommandGroup {
+		pathParts = append(pathParts,
+			d.Options[0].Name,
+			d.Options[0].Options[0].Name,
+		)
+		options = r.buildOptionsMap(d.Options[0].Options[0].Options)
+	}
+
+	return &handlerData{
+		path: strings.Join(pathParts, ":"),
+		opts: options,
+	}
 }
 
 func (r *Router) buildOptionsMap(options []*DiscordCmdOption) map[string]*DiscordCmdOption {
